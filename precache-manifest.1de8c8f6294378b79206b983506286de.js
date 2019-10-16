7caa2420815843977a3131712f2feb671014ed28
mditbbte <andrii.ds.kor@gmail.com>
2019-10-17 00:47:19 +0300

    Updates

---
+++ precache-manifest.1de8c8f6294378b79206b983506286de.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "406cef344e9e28739ed9284ef5068fbe",
+    "url": "/goit-react-hw-04-components/index.html"
+  },
+  {
+    "revision": "9201d7a1356de6534c02",
+    "url": "/goit-react-hw-04-components/static/css/3.a52718a8.chunk.css"
+  },
+  {
+    "revision": "1e7ee18471f25271486c",
+    "url": "/goit-react-hw-04-components/static/css/4.c355b9bb.chunk.css"
+  },
+  {
+    "revision": "6b63b1b97fe1dbc4f0d6",
+    "url": "/goit-react-hw-04-components/static/css/5.124834bc.chunk.css"
+  },
+  {
+    "revision": "661959dba4f9c6d9e566",
+    "url": "/goit-react-hw-04-components/static/css/main.2151025d.chunk.css"
+  },
+  {
+    "revision": "2177bc49b99c682b98a1",
+    "url": "/goit-react-hw-04-components/static/js/2.cd4b0f3e.chunk.js"
+  },
+  {
+    "revision": "9201d7a1356de6534c02",
+    "url": "/goit-react-hw-04-components/static/js/3.2b1f72de.chunk.js"
+  },
+  {
+    "revision": "1e7ee18471f25271486c",
+    "url": "/goit-react-hw-04-components/static/js/4.68b9841d.chunk.js"
+  },
+  {
+    "revision": "6b63b1b97fe1dbc4f0d6",
+    "url": "/goit-react-hw-04-components/static/js/5.ff4965f5.chunk.js"
+  },
+  {
+    "revision": "661959dba4f9c6d9e566",
+    "url": "/goit-react-hw-04-components/static/js/main.13eb4bb0.chunk.js"
+  },
+  {
+    "revision": "09788e0b43921310dd95",
+    "url": "/goit-react-hw-04-components/static/js/runtime-main.ba76de7c.js"
+  }
+]);
